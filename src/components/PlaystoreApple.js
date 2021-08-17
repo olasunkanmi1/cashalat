d11d3f86ed0b4165c0192903f6309a89dbd698c0
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-08-17 00:14:53 -0100

    commit

--- src/components/PlaystoreApple.js
+++ src/components/PlaystoreApple.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+//styles
+
 
 const PlaystoreApple = () => {
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     )
 }
 
